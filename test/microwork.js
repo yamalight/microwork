f8d35ab1c36f651e2b0f374f0a56a43af35882f7
Tim Ermilov <yamalight@gmail.com>
2016-02-03 14:40:20 +0100

    fix coverage generation

---
+++ test/microwork.js
@@ -0,0 +1,145 @@
+import test from 'tape';
+import Microwork from '../src';
+
+test('Microwork', it => {
+    it.test('  -> should deliver message from one service to another', async (t) => {
+        const exchange = 'master.to.runner';
+        // create master and runner services
+        const master = new Microwork({host: 'docker.dev', exchange});
+        const runner = new Microwork({host: 'docker.dev', exchange});
+        // test topic and message
+        const topic = 'test.path';
+        const message = {hello: 'world'};
+        // test consumer that should receive message
+        const consumer = async (msg) => {
+            t.deepEqual(message, msg, '# should get correct incoming object');
+            await master.stop();
+            await runner.stop();
+            t.end();
+        };
+        // add subscription
+        await runner.subscribe(topic, consumer);
+        // send message
+        await master.send(topic, message);
+    });
+
+    it.test('  -> should get reply for master query from runner', async (t) => {
+        const exchange = 'master.runner.reply';
+        // create master and runner
+        const master = new Microwork({host: 'docker.dev', exchange});
+        const runner = new Microwork({host: 'docker.dev', exchange});
+        // test topic and message
+        const topic = 'test.request';
+        const message = 'ping';
+        // test consumer that should receive message
+        const consumer = (msg, reply) => {
+            t.equal(msg, 'ping', '# should get correct incoming message');
+            reply(topic + '.response', 'pong');
+        };
+        // subscribe for reply
+        await master.subscribe(topic + '.response', async (msg) => {
+            t.equal(msg, 'pong', '# should get correct reply');
+            await master.stop();
+            await runner.stop();
+            t.end();
+        }, {durable: true, exclusive: true});
+        // subscribe for request
+        await runner.subscribe(topic, consumer);
+        // send message
+        await master.send(topic, message);
+    });
+
+    it.test('  -> should use round-robin to distribute tasks between three runners', async (t) => {
+        t.plan(6);
+        const exchange = 'master.multi.runners';
+        // create master and three runners
+        const master = new Microwork({host: 'docker.dev', exchange});
+        const runner = new Microwork({host: 'docker.dev', exchange});
+        const runnerTwo = new Microwork({host: 'docker.dev', exchange});
+        const runnerThree = new Microwork({host: 'docker.dev', exchange});
+        // test topic and message
+        const topic = 'test.request';
+        const messages = ['ping', 'ping_two', 'ping_three'];
+        const replies = ['pong', 'pong_two', 'ping_three'];
+        const repliesCheck = [...replies];
+        // test consumer that should receive message
+        const consumer = async (idx, msg, reply) => {
+            t.equal(msg, messages[idx], '# should get correct incoming message');
+            reply(topic + '.response', replies[idx]);
+        };
+        // subscribe for reply
+        await master.subscribe(topic + '.response', async (msg) => {
+            // check
+            const index = repliesCheck.indexOf(msg);
+            t.notEqual(index, -1, '# should get correct reply');
+            // remove from array
+            repliesCheck.splice(index, 1);
+            // cleanup after last msg
+            if (repliesCheck.length === 0) {
+                await master.stop();
+                await runner.stop();
+                await runnerTwo.stop();
+                await runnerThree.stop();
+            }
+        }, {durable: true, exclusive: true});
+
+        // add subscriptions for consumers
+        await runner.subscribe(topic, consumer.bind(null, 0));
+        await runnerTwo.subscribe(topic, consumer.bind(null, 1));
+        await runnerThree.subscribe(topic, consumer.bind(null, 2));
+        // send messages
+        await master.send(topic, messages[0]);
+        await master.send(topic, messages[1]);
+        await master.send(topic, messages[2]);
+    });
+
+    it.test('  -> should unsubscribe correctly', async (t) => {
+        t.plan(8);
+        const exchange = 'master.runner.unsubscribe';
+        // create master and runner
+        const master = new Microwork({host: 'docker.dev', exchange});
+        const runner = new Microwork({host: 'docker.dev', exchange});
+        const runnerTwo = new Microwork({host: 'docker.dev', exchange});
+        // test topic and message
+        const topic = 'test.request';
+        const message = 'ping';
+        const replies = ['pong', 'pong2'];
+        let repliesCount = 4;
+        // test consumer that should receive message
+        const consumer = (msg, reply) => {
+            t.equal(msg, 'ping', '# should get correct incoming message');
+            reply(topic + '.response', 'pong');
+        };
+        const consumerTwo = async (msg, reply) => {
+            t.equal(msg, 'ping', '# should get correct incoming message in second runner');
+            reply(topic + '.response', 'pong2');
+        };
+        // subscribe for reply
+        await master.subscribe(topic + '.response', async (msg) => {
+            t.notEqual(replies.indexOf(msg), -1, '# should get correct reply');
+            repliesCount--;
+            if (msg === 'pong2') {
+                // unsubscribe second runner
+                await runnerTwo.unsubscribe(topic);
+                // remove second answer
+                replies.splice(1, 1);
+                // send two more messages
+                await master.send(topic, message);
+                await master.send(topic, message);
+                return;
+            }
+            if (repliesCount === 0) {
+                await master.stop();
+                await runner.stop();
+                await runnerTwo.stop();
+                t.end();
+            }
+        }, {durable: true, exclusive: true});
+        // subscribe for requests
+        await runner.subscribe(topic, consumer);
+        await runnerTwo.subscribe(topic, consumerTwo);
+        // send message
+        await master.send(topic, message);
+        await master.send(topic, message);
+    });
+});
